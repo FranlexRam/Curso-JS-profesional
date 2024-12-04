0cbf46b75134461423f002f8c129f336c4e748e6
Franlex Ramirez <franlex.ramirez@gmail.com>
2024-12-03 22:42:47 -0400

    Practicando comillas invertidas

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 alert("Si funciona!")
+
+
+//Tipos de variables
+//let edad = 18;
+//console.log(edad);
+var vivo = true;
+const NOMBRE = "Jab"; //No se puede modificar
+
+
+//Usar las variables invertidas.
+let nombre1 = 'Jab';
+let nombre2 = 'Eduardo';
+let actual = new Date().getFullYear();
+let edad = 28;
+
+let NOMBRES = `Mi nombre es ${nombre2} y el profesor es ${nombre1}.
+Tengo ${edad} a{os y mi fecha de nacimiento es ${calcularNacimiento ()}`;
+
+document.querySelector("body").innerHTML=`
+<div>
+    <p> 
+        Mi nombre es <b>${nombre2}</b> y el profesor es ${nombre1}.<br>
+       Tengo ${edad} a{os y mi fecha de nacimiento es ${calcularNacimiento ()} 
+    </p>
+</div>
+`
+
+function calcularNacimiento() {
+    return actual - edad;
+}
