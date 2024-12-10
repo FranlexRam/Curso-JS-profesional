f0e6e2852af18fb57d353ab90e6c3bf8b1e6ae6f
Franlex Ramirez <franlex.ramirez@gmail.com>
2024-12-10 15:55:24 -0400

    Actividad #1: Operadores.

--- Operadores y expresiones/index.js
+++ Operadores y expresiones/index.js
@@ -4,8 +4,9 @@ let numero2 = 3;
 let nombre = "Franlex";
 let edad = 18;
 
-(nombre === "Franlex")
-? dibujar("Si, soy yo")
+//Operadores ternarios:
+(nombre === "Franlex" || edad===17)
+? dibujar("Si, soy yo.")
 : dibujar("NO soy Franlex")
 numero1+=10;
 dibujar(++numero1) //incremente 1 unidad.
@@ -16,3 +17,10 @@ function dibujar(valor) {
     caja.innerHTML+=`<div>${valor}</div>`
     
 }
+
+let vivo=true;
+let existo;
+
+(!vivo)
+? dibujar("Hola, yo existo.")
+: dibujar("Sorry, no existo.")
