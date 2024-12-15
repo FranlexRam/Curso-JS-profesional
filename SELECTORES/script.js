5049eb59a5e332b0b47e8b73bffec0c8f04f4b31
Franlex Ramirez <franlex.ramirez@gmail.com>
2024-12-14 21:14:02 -0400

    Detalles de los selectores.

--- SELECTORES/script.js
+++ SELECTORES/script.js
@@ -1,1 +1,10 @@
-document.getElementsByClassName("caja")[3].innerHTML=`CUATRO`
+//Usando un selector mas largo (getElementsByClassName)
+document.getElementsByClassName("caja")[2].getElementsByClassName("contenido")[0].innerHTML=`TRES`
+
+//Usando un selector corto (querySelector)
+document.querySelector(".caja .query").innerHTML=`UNO`
+
+let cajas = document.getElementsByClassName("caja");
+document.getElementById("seccion").innerHTML+=`
+<h1>Hay ${cajas.length} elementos con la clase: caja.</h1>
+`
