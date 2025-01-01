997b37f35d3c3eee6bf4c82d723a6c460cc534c6
Franlex Ramirez <franlex.ramirez@gmail.com>
2025-01-01 18:19:50 -0400

    Aprendiendo el insertAdjacentHTML.

--- InnerHTML/index.js
+++ InnerHTML/index.js
@@ -1,10 +1,10 @@
 const CAJA = document.querySelector(".caja");
 
-CAJA.innerHTML+=`<button>Aceptar</button>`;
-document.querySelector("button").addEventListener("click", saludar);
-CAJA.innerHTML+=`<button>Cancelar</button>`
-document.querySelectorAll("button")[1].addEventListener("click", saludar); 
+for (let k = 0; k < 10; k++) {
+    CAJA.insertAdjacentHTML("beforeend",`<button class="bt${k}">Button #${k+1}</button>`)
+    document.querySelector(".bt"+k).addEventListener("click", saludar);
+}
 
 function saludar() {
-    alert("Hola, todo bien?");
+    alert(`Hola, todo bien?`);
 }
