2bce87502018721f668a220f057de0be6b748f0f
Franlex Ramirez <franlex.ramirez@gmail.com>
2025-01-11 23:11:55 -0400

    Traer nombres de Pokemon desde la API 2

---
+++ APIs con JS/index.js
@@ -0,0 +1,36 @@
+let urlAnterior;
+let urlPosterior="https://pokeapi.co/api/v2/pokemon?offset=0&limit=20";
+let contador=0;
+
+siguientePagina();
+
+function siguientePagina() {
+    cargarDatos(urlPosterior);
+}
+
+function anteriorPagina() {
+    cargarDatos(urlAnterior);
+}
+
+function cargarDatos(url) {
+    fetch(url)
+    .then(respuesta => respuesta.json())
+    .then(valor => mostrar(valor));
+}
+
+function mostrar(dato) {
+    document.querySelector(".caja").innerHTML="";
+    document.querySelector(".caja").insertAdjacentHTML("beforeend", `
+        <div class="nombre">
+            <h2>Pokemon</h2>
+            <div>(del ${contador+1} al ${contador+20})</div>
+        </div>`);
+        dato.results.map(valor=>
+            document.querySelector(".caja").insertAdjacentHTML("beforeend", `
+                <div class="linea">
+                    ${++contador} - <span class="pokemon">${valor.name}</span>
+                </div>
+                `)
+        )
+        console.log(dato);
+}
